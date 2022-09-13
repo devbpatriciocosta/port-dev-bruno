d4cb66ec13f344a91461bfcd30f416dc2d8384d3
Bruno Patrício <bpatriciocosta@gmail.com>
2022-09-13 11:02:16 -0300

    Finishing Education

--- src/components/layout/Education.jsx
+++ src/components/layout/Education.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 
 import H1 from '../../typograph/H1'
+import H5 from '../../typograph/H5'
 
 const StyledEducation = styled.div`
   height: 100vh;
@@ -10,6 +11,21 @@ const StyledEducationTitle = styled.div`
   display: flex;
   justify-content: center;
   padding-top: 10vh;
+  padding-bottom: 10vh;
+`
+
+const StyledEducationText = styled.div`
+  background-color: #fec350;
+  height: 6rem;
+  width: 40rem;
+  margin: 2rem 39rem;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  gap: 15px;
 `
 
 export default function Education() {
@@ -18,6 +34,21 @@ export default function Education() {
       <StyledEducationTitle>
         <H1>Education</H1>
       </StyledEducationTitle>
+      <StyledEducationText>
+        <H5>
+          Web Developer certification in: <br />
+          JavaScript, HTML5, CSS3, ReactJS, NodeJS, <br /> NextJS, Styled-Components, SQL/NO-SQL
+        </H5>
+      </StyledEducationText>
+      <StyledEducationText>
+        <H5>Ph.D. in Food Engineering focused in Biodegradadble Food Packaging</H5>
+      </StyledEducationText>
+      <StyledEducationText>
+        <H5>English Fluency Certified by TOEFL Test</H5>
+      </StyledEducationText>
+      <StyledEducationText>
+        <H5>French Intermidiate Level Certified by “Francês Simplificado”</H5>
+      </StyledEducationText>
     </StyledEducation>
   )
 }
