1bcca6622ddf424c3763643f56e2166084eba1d2
Bruno Patrício <bpatriciocosta@gmail.com>
2022-10-03 15:07:27 -0300

    Criado index

---
+++ pages/api/post/index.js
@@ -0,0 +1,5 @@
+import { withIronSessionApiRout } from 'iron-session/next'
+
+import createHandler from '../../../lib/middlewares/nextConnect'
+import validade from '../../../lib/middlewares/validation'
+
