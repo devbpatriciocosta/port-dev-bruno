6659736d761ed5ed07eb693bd94a85f0573d1f54
Bruno Patrício <bpatriciocosta@gmail.com>
2022-10-03 15:06:53 -0300

    Criado post Model

---
+++ modules/post/post.model.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose'
+
+const PostSchema = new mongoose.Schema({
+  text: { type: String, required: true, maxlength: 256 },
+  createdDate: { type: Date, required: true },
+  user: { type: String },
+  createdBy: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Name' }
+})
+
+export default mongoose.models.Post || mongoose.model('Post', PostSchema)
