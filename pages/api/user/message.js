ccb18fb20348ee6d1e0dac038bca276f98eaec35
Bruno Patrício <bpatriciocosta@gmail.com>
2022-10-14 16:48:32 -0300

    Criado autenticação

--- pages/api/user/message.js
+++ pages/api/user/message.js
@@ -1,9 +1,12 @@
 import Joi from 'joi'
+import { withIronSessionApiRoute } from 'iron-session/next'
 import createHandler from '../../../lib/middlewares/nextConnect'
 
 import validate from '../../../lib/middlewares/validation'
 
 import { nameUser } from '../../../modules/user/user.service'
+
+import { ironConfig } from '../../../lib/middlewares/ironSession'
 
 const postSchema = Joi.object({
   Name: Joi.string().required().max(50),
@@ -16,11 +19,16 @@ const message = createHandler()
 message.post(validate({ body: postSchema }), async (req, res) => {
   try {
     const message = await nameUser(req.body)
-    res.status(201).json(message)
+    req.session.user = {
+      id: message._id,
+      user: message.Name
+    }
+    await req.session.save()
+    res.send({ ok: true })
   } catch (err) {
     console.error(err)
     throw err
   }
 })
 
-export default message
+export default withIronSessionApiRoute(message, ironConfig)
